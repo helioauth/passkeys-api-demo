0873162c49a86e12965a4251cdd04afd97d562c3
Victor Stanchev <st.victor@gmail.com>
2024-05-26 10:11:50 +0300

    Add response status to exception

--- src/main/java/com/helioauth/passkeys/demo/service/exception/UsernameAlreadyRegisteredException.java
+++ src/main/java/com/helioauth/passkeys/demo/service/exception/UsernameAlreadyRegisteredException.java
@@ -1,5 +1,9 @@
 package com.helioauth.passkeys.demo.service.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Email already exists")
 public class UsernameAlreadyRegisteredException extends RuntimeException {
 
 }
