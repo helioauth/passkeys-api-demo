f1946c354d0869a450e2d9953269774a07920b24
Victor Stanchev <st.victor@gmail.com>
2024-05-26 10:13:20 +0300

    Exclude signin paths from security config

--- src/main/java/com/helioauth/passkeys/demo/config/WebSecurityConfig.java
+++ src/main/java/com/helioauth/passkeys/demo/config/WebSecurityConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,13 +22,15 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/create-credential", "/register-credential").permitAll()
                         .requestMatchers("/js/**", "/css/**", "/img/**", "/favicon.ico").permitAll()
-
-//                        .requestMatchers("/", "/dashboard","/error").fullyAuthenticated()
+                        .requestMatchers("/create-credential", "/register-credential").permitAll()
+                        .requestMatchers("/signin-credential-options", "/signin-validate-key").permitAll()
 
                         .anyRequest().fullyAuthenticated()
                 )
+//                .addFilterBefore((servletRequest, servletResponse, filterChain) -> {
+//                    filterChain.doFilter(servletRequest, servletResponse);
+//                }, UsernamePasswordAuthenticationFilter.class)
                 .formLogin((form) -> form
                         .loginPage("/signin")
                         .defaultSuccessUrl("/")
