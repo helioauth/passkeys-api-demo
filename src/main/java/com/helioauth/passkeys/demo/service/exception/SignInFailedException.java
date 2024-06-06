41ef11f2ab78c3045ebb7e8aafded9bb827d9b1d
Victor Stanchev <st.victor@gmail.com>
2024-06-06 11:22:32 +0300

    Default error message for login failure

--- src/main/java/com/helioauth/passkeys/demo/service/exception/SignInFailedException.java
+++ src/main/java/com/helioauth/passkeys/demo/service/exception/SignInFailedException.java
@@ -1,9 +1,7 @@
 package com.helioauth.passkeys.demo.service.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Couldn't find an account with this email and passkey.")
 public class SignInFailedException extends RuntimeException {
-
+    public SignInFailedException() {
+        super("Couldn't find an account with this email and passkey.");
+    }
 }
