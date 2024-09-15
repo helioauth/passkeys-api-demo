0e2abcaeaadfaa8edabd64ad3dd1cdd8de89a9a9
Victor Stanchev <st.victor@gmail.com>
2024-09-15 19:14:52 +0300

    Add database-backed UserDetails and UserDetailsService

---
+++ src/main/java/com/helioauth/passkeys/demo/service/DatabaseUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.helioauth.passkeys.demo.service;
+
+import com.helioauth.passkeys.demo.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DatabaseUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByName(username)
+            .map(DatabaseUserDetails::new)
+            .orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+}
