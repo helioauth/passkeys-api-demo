c1b1f744af78d536347093278100106a5f2e9349
Victor Stanchev <st.victor@gmail.com>
2024-09-17 22:18:47 +0300

    Make PasskeyAuthenticationProvider to use the Passkeys API

--- src/main/java/com/helioauth/passkeys/demo/service/PasskeyAuthenticationProvider.java
+++ src/main/java/com/helioauth/passkeys/demo/service/PasskeyAuthenticationProvider.java
@@ -1,35 +1,41 @@
 package com.helioauth.passkeys.demo.service;
 
+import com.helioauth.passkeys.demo.client.PasskeyApiException;
+import com.helioauth.passkeys.demo.client.PasskeysApiClient;
+import com.helioauth.passkeys.demo.client.SignInFinishRequest;
 import com.helioauth.passkeys.demo.contract.SignInValidateKeyRequest;
-import com.helioauth.passkeys.demo.service.exception.SignInFailedException;
+import com.helioauth.passkeys.demo.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
 
-import java.util.List;
-
+@Component
 @Slf4j
+@RequiredArgsConstructor
 public class PasskeyAuthenticationProvider implements AuthenticationProvider {
-    private final UserSignInService userSignInService;
-
-    public PasskeyAuthenticationProvider(UserSignInService userSignInService) {
-        this.userSignInService = userSignInService;
-    }
+    private final UserRepository userRepository;
+    private final PasskeysApiClient passkeysApiClient;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         SignInValidateKeyRequest request = (SignInValidateKeyRequest) authentication.getCredentials();
 
-        // TODO Fix user handle to be the same on all
         try {
-            String loggedInUsername = userSignInService.finishAssertion(request.getRequestId(), request.getPublicKeyCredentialWithAssertion());
+            var response = passkeysApiClient.signInFinish(
+                new SignInFinishRequest(request.getRequestId(), request.getPublicKeyCredentialWithAssertion())
+            );
 
-            return new PasskeyAuthenticationToken(loggedInUsername, request, List.of(new SimpleGrantedAuthority("USER")));
-        } catch (SignInFailedException e) {
+            return userRepository.findByName(response.username())
+                .map(PasskeyAuthenticationToken::authenticated)
+                .orElseThrow(() -> new BadCredentialsException("User not found"));
+
+        } catch (PasskeyApiException e) {
+            log.error(e.getMessage(), e);
             throw new BadCredentialsException(e.getMessage());
         } catch (Exception e) {
             log.error(e.getMessage(), e);
