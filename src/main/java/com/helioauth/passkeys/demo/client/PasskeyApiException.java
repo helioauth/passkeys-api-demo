f43a6bc8b4f9dec4569f8aa01bc8049c10949e34
Victor Stanchev <st.victor@gmail.com>
2024-10-27 17:36:44 +0200

    Remove overloaded constructor from PasskeyApiException

--- src/main/java/com/helioauth/passkeys/demo/client/PasskeyApiException.java
+++ src/main/java/com/helioauth/passkeys/demo/client/PasskeyApiException.java
@@ -4,8 +4,4 @@ public class PasskeyApiException extends RuntimeException {
     public PasskeyApiException(String s) {
         super(s);
     }
-
-    public PasskeyApiException(String s, Throwable throwable) {
-        super(s, throwable);
-    }
 }
