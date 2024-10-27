d2c3d9b7e2aba342c209aac89862372d533520b4
Victor Stanchev <st.victor@gmail.com>
2024-10-27 09:18:13 +0200

    Refactor SignInFinishRequest to record type.

--- src/main/java/com/helioauth/passkeys/demo/client/SignInFinishRequest.java
+++ src/main/java/com/helioauth/passkeys/demo/client/SignInFinishRequest.java
@@ -1,11 +1,6 @@
 package com.helioauth.passkeys.demo.client;
 
-public class SignInFinishRequest {
-    public String requestId;
-    public String publicKeyCredentialWithAssertion;
-
-    public SignInFinishRequest(String requestId, String publicKeyCredentialWithAssertion) {
-        this.requestId = requestId;
-        this.publicKeyCredentialWithAssertion = publicKeyCredentialWithAssertion;
-    }
-}
+public record SignInFinishRequest(
+    String requestId,
+    String publicKeyCredentialWithAssertion
+) {}
