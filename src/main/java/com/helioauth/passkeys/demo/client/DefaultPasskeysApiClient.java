6d7d5d8974ead8a16d966435d20c4371281f5306
Victor Stanchev <st.victor@gmail.com>
2025-04-12 17:45:03 +0300

    Add API key and app ID support to PasskeysApiClient

--- src/main/java/com/helioauth/passkeys/demo/client/DefaultPasskeysApiClient.java
+++ src/main/java/com/helioauth/passkeys/demo/client/DefaultPasskeysApiClient.java
@@ -12,14 +12,19 @@ public class DefaultPasskeysApiClient implements PasskeysApiClient {
     public static final String SIGNUP_FINISH_ENDPOINT = "v1/signup/finish";
     public static final String SIGNIN_FINISH_ENDPOINT = "v1/signin/finish";
     public static final String LIST_PASSKEYS_ENDPOINT = "v1/users/%s/credentials";
+    public static final String X_API_KEY = "X-Api-Key";
 
     private final OkHttpClient client = new OkHttpClient();
     private final String apiUrl;
+    private final String appId;
+    private final String apiKey;
     private final ObjectMapper objectMapper;
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
-    public DefaultPasskeysApiClient(String apiUrl, ObjectMapper objectMapper) {
+    public DefaultPasskeysApiClient(String apiUrl, String appId, String apiKey, ObjectMapper objectMapper) {
         this.apiUrl = apiUrl.endsWith("/") ? apiUrl : apiUrl + "/";
+        this.appId = appId;
+        this.apiKey = apiKey;
         this.objectMapper = objectMapper;
     }
 
@@ -44,10 +49,12 @@ public class DefaultPasskeysApiClient implements PasskeysApiClient {
     private String post(String json, String endpoint) throws IOException {
         RequestBody body = RequestBody.create(json, JSON);
 
-        Request request = new Request.Builder()
+        Request.Builder requestBuilder = new Request.Builder()
             .url(apiUrl + endpoint)
-            .post(body)
-            .build();
+            .addHeader(X_API_KEY, apiKey)
+            .post(body);
+
+        Request request = requestBuilder.build();
 
         try (Response response = client.newCall(request).execute()) {
             if (!response.isSuccessful()) {
